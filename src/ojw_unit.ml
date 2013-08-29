6d52dd7a931ff29fbfdfaa4cc5bda9df0d277a27
shakurra <charly.n.chevalier@gmail.com>
2013-08-29 15:18:30 +0200

    add some checks for int_of_pxstring

--- src/ojw_unit.ml
+++ src/ojw_unit.ml
@@ -6,10 +6,12 @@
 let subs_suffix s n = String.sub s 0 ((String.length s) - n)
 
 let int_of_pxstring px =
-  match (Js.to_string px) with
-    | s when s = (subs_suffix s 2)^"px" ->
-        int_of_float (float_of_string (subs_suffix s 2))
-    | _ -> 0 (* raise exception ? *)
+  if not (String.length (Js.to_string px) > 2) then 0
+  else
+    (match (Js.to_string px) with
+       | s when s = (subs_suffix s 2)^"px" ->
+           int_of_float (float_of_string (subs_suffix s 2))
+       | _ -> 0 (* raise exception ? *))
 
 let pxstring_of_int px =
   Js.string ((string_of_int px)^"px")
