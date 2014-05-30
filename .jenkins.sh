a6cd4d4915c53c18356fe043b9694be47c965f0a
Drup <drupyog@zoho.com>
2014-05-30 20:21:20 +0200

    Use normal dev version of jquery.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,5 @@
 opam pin ojwidgets .
-opam pin ojquery https://github.com/ocsigen/ojquery.git#jenkins
+opam pin ojquery https://github.com/ocsigen/ojquery.git#master
 opam install --deps-only ojwidgets
 opam install --verbose ojwidgets
 opam remove --verbose ojwidgets
