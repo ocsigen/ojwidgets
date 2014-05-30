8e2c1337d1f34361fde051c44b876952789be1d2
Drup <drupyog@zoho.com>
2014-05-30 20:26:11 +0200

    prepare release

--- .jenkins.sh
+++
@@ -1,5 +0,0 @@
-opam pin ojwidgets .
-opam pin ojquery https://github.com/ocsigen/ojquery.git#master
-opam install --deps-only ojwidgets
-opam install --verbose ojwidgets
-opam remove --verbose ojwidgets
