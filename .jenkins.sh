55395eb0ef339f8f00a132f29e258df6e15971ca
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-12 23:03:52 +0200

    Update .jenkins.sh

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,5 @@
-opam pin ojwidgets .
-opam pin ojquery https://github.com/ocsigen/ojquery.git#master
+opam pin add --no-action ojwidgets .
+opam pin add --no-action ojquery https://github.com/ocsigen/ojquery.git#master
 opam install --deps-only ojwidgets
 opam install --verbose ojwidgets
 opam remove --verbose ojwidgets
