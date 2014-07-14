e68a7d9783c404a4c5b0d24db61fcde8bc504a83
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:24:11 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,15 @@
+
 opam pin add --no-action ojwidgets .
 opam pin add --no-action ojquery https://github.com/ocsigen/ojquery.git#master
 opam install --deps-only ojwidgets
 opam install --verbose ojwidgets
-opam remove --verbose ojwidgets
+
+do_build_doc () {
+  make wikidoc
+  cp -Rf _build/ojwidgets-api.wikidocdir/*.wiki $(API_DIR)
+  cp -Rf doc/manual-wiki/*.wiki $(MANUAL_SRC_DIR)/
+}
+
+do_remove () {
+  opam remove --verbose ojwidgets
+}
